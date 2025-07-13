e55ae447d9005f720b1cb1b9ce3f6a7c101c5d93
marcdevceo <marcdevceo@icloud.com>
2025-07-12 20:20:37 -0400

    removed old import of mellc-ui-kit in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import 'mellc-ui-kit';
 import './globals.css';
 import '../styles/project-colors.css';
 import { Footer, NavBar } from '@/mellc-ui-kit';
